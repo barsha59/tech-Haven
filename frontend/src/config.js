4bbbe10be7ad357f40df814796b496acf1aa0264
barsha karna <barsha@kali.barsha.com>
2026-02-22 10:12:38 +0545

    Updating frontend/config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 // src/config.js
-const API_URL = "http://127.0.0.1:5000";  // Correct port
+const API_URL = "http://98.93.165.118:5000";  // Correct port
 const STRIPE_PUBLISHABLE_KEY =
   "pk_test_51Sl1TM2YdULA0kvG2JENo4pXukqssFkAHNfEYoeqzdGrkIxBLJh8YirmBdusf6yaCJxQXWq1W7usBupkDNupbPHf00AVabIsFL";
 
